46a926e35e5ca1cc027453a69142ffed95838a62
khlebov Kirill <khlebov79@gmail.com>
2023-09-05 14:34:17 +0300

    fix: swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+//Burger menu
+
 const burgerButtonElement = document.querySelector('.header__burger-button');
 const overlayMenuElement = document.querySelector('.header__overlay-menu');
 
@@ -5,6 +7,16 @@ const overlayMenuElement = document.querySelector('.header__overlay-menu');
 burgerButtonElement.addEventListener('click', () => {
     burgerButtonElement.classList.toggle('is-active')
     overlayMenuElement.classList.toggle('is-active')
-} )
+})
 
+//Swiper
 
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.advantages__pagination',
+    },
+    navigation: {
+        nextEl: '.advantages__slider-button--next',
+        prevEl: '.advantages__slider-button--prev',
+    },
+});
